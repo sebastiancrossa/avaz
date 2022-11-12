eb627184370abae75730b4642716c648c77efca9
Sebastian Crossa <sebastiancrossa@hotmail.com>
2022-11-11 20:28:34 -0600

    Initial commit + repo cleanup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1>avaz</h1>
+    </div>
+  );
+};
+
+export default Home;
